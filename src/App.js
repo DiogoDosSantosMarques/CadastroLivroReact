3631ff4def4c0fe4fa8752e1e7bb3f55164221f1
DiogoDosSantosMarques <marques.gogosantos@gmail.com>
2024-05-28 10:23:40 -0300

    resta o botao de edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Footer from './componentes/Footer';
 import Main from './componentes/Main';
@@ -10,14 +10,8 @@ function App() {
   return (
     <div className="App">
 
-      <header>
-        <h2>Cadastro de Livro</h2>
-      </header>
-
       <Main />
 
-      <Footer />
-      
     </div>
   );
 }
--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,17 +1,64 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
+
+import "./Main.css";  // Importe o arquivo CSS
 
 const Main = () => {
+
+    const [nome, setNome] = useState("")
+    const [paginas, setPaginas] = useState("")
+    const [autor, setAutor] = useState("")
+
+    
+    const [livros, setLivros] = useState(() => {
+        const savedLivros = localStorage.getItem("livros")
+        return savedLivros ? JSON.parse(savedLivros) : []
+    })
+
+    // Salvar dados no localStorage sempre que a lista de livros mudar
+
+    useEffect(() => {
+
+        localStorage.setItem("livros", JSON.stringify(livros));
+
+
+    }, [livros])
+    
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+
+        setLivros([...livros, {nome, paginas, autor}])
+
+        setNome("");
+        setPaginas("");
+        setAutor("");
+    }
+
+
+    const deleteSubmit = (index) => {
+
+        const newLivro = livros.filter((_, i) => i !== index)
+
+        setLivros(newLivro)
+        
+    }
+
+
+   
 
 
     return(
 
-        <main>
+        <div className="container">
+
+<h2>Cadastro de Livro</h2>
 
 <section id="campos">
-            <form id="formulario"> 
-                <input class="campos" type="text" name="nome" placeholder="Nome do Livro" /> 
-                <input class="campos" type="text" name="paginas" placeholder="Páginas do Livro" /> 
-                <input class="campos" type="text" name="autor" placeholder="Autor do Livro" />
+            <form id="formulario" onSubmit={handleSubmit}> 
+                <input class="campos" type="text" name="nome" placeholder="Nome do Livro" value={nome} onChange={(e) => setNome(e.target.value)} /> 
+                <input class="campos" type="text" name="paginas" placeholder="Páginas do Livro" value={paginas} onChange={(e) => setPaginas(e.target.value)} /> 
+                <input class="campos" type="text" name="autor" placeholder="Autor do Livro" value={autor} onChange={(e) => setAutor(e.target.value)} />
                 
                 <button>Salvar</button>
                 
@@ -25,15 +72,37 @@ const Main = () => {
                 <div class="nome">Nome</div>
                 <div class="paginas">Páginas</div>
                 <div class="autor">Autor</div>
-                <div class="acoes">Ações</div>
+                
             </div>
 
 
             <div id="body_tabela">
+
+        {livros.map((livro, index) => (
+
+            <>
+
+            <div className="nomes" key={index}>
+
+            <div className="nome">{livro.nome}</div>
+              <div className="paginas">{livro.paginas}</div>
+              <div className="autor">{livro.autor}</div>
+              
+              
+            </div>
+
+            <button onClick={() => deleteSubmit(index)} className="btn">Deletar</button>
+             <button className="btn-edit">Editar</button>
+
+            </>
+
+        ))}
+        
+
             </div>
         </section>
 
-        </main>
+        </div>
 
     )
 }
